9722d0a49a5ac3c66eb48841d294d3e92aaed0b4
Jack <wangjievibeke@foxmail.com>
2022-02-01 04:59:51 +0700

    package bf

--- bf.go
+++ bf.go
@@ -20,13 +20,12 @@
  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
  THE SOFTWARE.
 */
-package main
+package bf
 
 import (
 	"bufio"
 	"errors"
 	"fmt"
-	"io/ioutil"
 	"os"
 )
 
@@ -121,26 +120,6 @@ func execute_bf(program []Instruction) {
 }
 
 func RunBF(fileContents []byte) {
-	program, err := compile_bf(string(fileContents))
-	if err != nil {
-		fmt.Println(err)
-		return
-	}
-	execute_bf(program)
-}
-
-func main() {
-	args := os.Args
-	if len(args) != 2 {
-		fmt.Printf("Usage: %s filename\n", args[0])
-		return
-	}
-	filename := args[1]
-	fileContents, err := ioutil.ReadFile(filename)
-	if err != nil {
-		fmt.Printf("Error reading %s\n", filename)
-		return
-	}
 	program, err := compile_bf(string(fileContents))
 	if err != nil {
 		fmt.Println(err)
